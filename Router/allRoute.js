8c0326b38626426e790b7cb2aef320fa321e20cd
Neyliss <fenix425@live.fr>
2024-09-21 18:36:09 +0400

    modification accés

--- Router/allRoute.js
+++ Router/allRoute.js
@@ -3,17 +3,17 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/JS/auth/signin.js"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected","admin"], "/JS/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["admin"], "/JS/auth/singup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["employe", "admin"]),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["employe", "admin", "veterinaire"], "/JS/auth/changepassword.js"),
-    new Route("/contact", "Contact", "/pages/Reservation/contact.html", ["disconnected"], "/JS/Contact/contact.js"),
-    new Route("/jungle", "Jungle", "/pages/Habitation/jungle.html", ["disconnected"], "/JS/Habitat/animal.js"),
-    new Route("/marais", "Marais", "/pages/Habitation/marais.html", ["disconnected"], "/JS/Habitat/animal.js"),
-    new Route("/savane", "Savane", "/pages/Habitation/savane.html", ["disconnected"], "/JS/Habitat/animal.js"),
-    new Route("/service", "Service", "/pages/service.html", [],"/JS/service/service.js"),
-    new Route("/habitat", "Habitats", "/pages/habitat.html", [], "/JS/Habitat/habitat.js"),
-    new Route("/veterinaire", "Espace Veterniaire", "/pages/veterinaire.html", ["veterinaire"], "/JS/veto/veterinaire.js"),
+    new Route("/contact", "Contact", "/pages/Reservation/contact.html", ["disconnected", "admin"], "/JS/Contact/contact.js"),
+    new Route("/jungle", "Jungle", "/pages/Habitation/jungle.html", ["disconnected", "admin"], "/JS/Habitat/animal.js"),
+    new Route("/marais", "Marais", "/pages/Habitation/marais.html", ["disconnected","admin"], "/JS/Habitat/animal.js"),
+    new Route("/savane", "Savane", "/pages/Habitation/savane.html", ["disconnected", "admin"], "/JS/Habitat/animal.js"),
+    new Route("/service", "Service", "/pages/service.html", [],"/JS/service/service.js","admin"),
+    new Route("/habitat", "Habitats", "/pages/habitat.html", [], "/JS/Habitat/habitat.js", "admin"),
+    new Route("/veterinaire", "Espace Veterniaire", "/pages/veterinaire.html", ["veterinaire", "admin"], "/JS/veto/veterinaire.js"),
     new Route("/avis", "Laisser un avis", "/pages/avis.html", [], "/JS/avis/avis.js"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
